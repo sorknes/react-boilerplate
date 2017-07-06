07e50f6c85ae1181a9ca86408ba16412f9367e17
Knut Sorknes <knut.sorknes@makingwaves.com>
2017-07-06 09:21:17 +0200

    Startet setting up react routing (v4)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,22 @@
 
 
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+// components
+import Header from './header/Header';
+import Home from './home/Home';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="test">
-        <h1>Hello World</h1>
-        <p>Lorm ipsum dolor sit amet</p>
+      <div className="container">
+        <Header />
+
+        <BrowserRouter>
+          {/* Each route is defined with Route */}
+          <Route path="/" component={Home}/>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+/*  ------------------------------------------------
+./src/components/header/Header.jsx
+---------------------------------------------------- */
+
+
+import React, { Component } from 'react';
+
+class Header extends React.Component {
+	render() {
+		return (
+			<h1>Hello World</h1>
+		);
+	}
+}
+
+export default Header;
