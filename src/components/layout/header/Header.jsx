71f88c4eb8cacbcaaf79c48768965331927d73ea
Knut Sorknes <knut.sorknes@makingwaves.com>
2017-08-15 16:00:07 +0200

    Added logo template file

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -9,6 +9,7 @@ import React, { Component } from 'react';
 
 /* components
 */
+import Logo from './logo/Logo';
 import Nav from './nav/Nav';
 
 class Header extends React.Component {
@@ -16,6 +17,8 @@ class Header extends React.Component {
 		return (
 			<div className="container-fluid">
 				<header id="main-header" className="" role="banner">
+					<Logo />
+
 					<Nav />
 				</header>
 			</div>
---
+++ src/components/layout/header/logo/Logo.jsx
@@ -0,0 +1,20 @@
+/* path to file: ./src/components/layout/header/logo/Logo.jsx
+*
+*  description: 
+*  ------------------------------------------------------------------------------------
+*/
+
+
+import React, { Component } from 'react';
+
+class Logo extends React.Component {
+	render() {
+		return(
+			<div>
+				logo
+			</div>
+		)
+	}
+}
+
+export default Logo;
